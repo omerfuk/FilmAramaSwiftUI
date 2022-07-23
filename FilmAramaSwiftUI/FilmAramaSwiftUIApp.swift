ca1c79fbffddbeb4b28ac3d1aaffe3212c004106
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-23 22:29:24 +0300

    Initial Commit

---
+++ FilmAramaSwiftUI/FilmAramaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAramaSwiftUIApp.swift
+//  FilmAramaSwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FilmAramaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
