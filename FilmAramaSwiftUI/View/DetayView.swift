267f3bfbba7ebce466b86409e26f8356d20dee69
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-24 15:19:59 +0300

    Application done due to the course

--- FilmAramaSwiftUI/View/DetayView.swift
+++ FilmAramaSwiftUI/View/DetayView.swift
@@ -16,7 +16,24 @@ struct DetayView: View {
     var body: some View {
         VStack{
             
-            Text(filmDetayViewModel.filmDetayi?.plot ?? "")
+            OzelImage(url: filmDetayViewModel.filmDetayi?.poster ?? "")
+                .frame(width: UIScreen.main.bounds.width * 0.6, height: 300)
+                .padding()
+            
+            Text(filmDetayViewModel.filmDetayi?.title ?? "Film ismi Gösterilecek")
+                .padding()
+                .foregroundColor(.blue)
+            
+            Text(filmDetayViewModel.filmDetayi?.plot ?? "Film Plotu Gösterilecek")
+                .padding()
+                .foregroundColor(.orange)
+            
+            Text("Yönetmen : \(filmDetayViewModel.filmDetayi?.director ?? "Yönetmen Gösterilecek")")
+                .padding()
+                .foregroundColor(.green)
+            
+            Text("Ödüller : \(filmDetayViewModel.filmDetayi?.awards ?? "Ödüller Gösterilecek")")
+                .padding()
             
         }
         .onAppear {
